624af6e4b4f6667944e9075b9dedce5be1a16fff
Jauho <6375822+jauha@users.noreply.github.com>
2018-02-22 12:25:37 +0200

    Update reader.js

--- reader.js
+++ reader.js
@@ -16,7 +16,7 @@ var options = {
         username: "<YOUR BOT NAME>",
         password: "oauth:<ADD YOUR OWN OAUTH HERE>"
     },
-    channels: ["#shroud"]
+    channels: ["#shroud"] // initial channel shouldn't matter, chose shroud since he was online most of the time during developement
 };
 
 var client = new tmi.client(options); // just creating this for the included request libs to reduce bloat
